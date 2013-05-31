52a2e817a1ed75b646e85f1b18ddaf2bafbe420d
vishnu.kanduri <vishnu.kanduri@maxsoftware.com.au>
2013-05-31 15:15:10 +1000

    added Btech Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tech::Application.config.secret_token = '759da97f3faf809c177e851cd453ad2de12af345e7ec3ba63a75f31a7fcb1890ac19feb183ad7942fc7b2643a53dd6b625cf0290fbce1f4d1e33945df6b93fab'
